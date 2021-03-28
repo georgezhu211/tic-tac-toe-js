fa05d94251a9f469451f9e8f78f7279d0fde6fd2
George <georgezhu211@gmail.com>
2021-03-28 17:08:19 -0500

    add display for game

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+/*
+Rules for Tic-Tac-Toe:
+----------------------
+game is player by 2 players
+  X and O
+on a 3x3 grid
+  an array with 9 slots
+  a div container wrap 9 boxes
+  each box correlates to one of the slots in array
+starting with X, each player takes a turn
+  begin the game with X mark
+marking spaces on the 3x3 grid
+  when clicking on empty space, fill with mark
+  update array
+  check for winner
+    3 in a row, declare the winner
+  check for draw
+    no more space left
+  switch player
+game ends
+restart to clear grid and repeat
+*/
+
+const TicTacToe =  (function() {
+  let gameBoard = ['0','1','2','3','4','5','6','7','8'];
+
+  
+})();
